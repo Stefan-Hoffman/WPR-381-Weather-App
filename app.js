f1b7f3119c528f14dca53d57af7af3a6567a4aff
Stefan-Hoffman <shoffman111tefan@gmail.com>
2023-05-31 21:03:04 +0200

    Added backend and some react

--- app.js
+++ app.js
@@ -1,47 +1,130 @@
-import React, { useState } from 'react';
+const express = require('express');
+const app = express();
+const axios = require('axios');
+
+const port = process.env.PORT;
+const HOST = process.env.HOST;
+const URL = process.env.URL;
+
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Create a Weather schema and model
+const weatherSchema = new mongoose.Schema({
+  zip: String,
+  data: Object,
+});
+
+const Weather = mongoose.model('Weather', weatherSchema);
+
+// Connect to MongoDB
+try{mongoose.connect(URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})}
+catch(error){
+  console.log(error);
+}
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+// GET route to fetch weather data
+function getWeather() 
+{app.get('/weather/:zip', async (req, res) => {
+  try {
+    const { zip } = req.params;
+    
+    // Check if weather data exists in MongoDB
+    const weather = await Weather.findOne({ zip });
+
+    if (weather) {
+      // If weather data exists, return it from the database
+      res.json(weather.data);
+    } else {
+      // Make a request to OpenWeatherMap API
+      const apiKey = process.env.WEATHERAPI;
+      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}`;
+      const response = await axios.get(apiUrl);
+      const weatherData = response.data;
+
+      // Save weather data to MongoDB
+      const newWeather = new Weather({
+        zip,
+        data: weatherData,
+      });
+      await newWeather.save();
+      res.json(weatherData);
+    }
+  } catch (error) {
+    res.status(404).json({ message: 'Weather data not found' });
+  }
+});
+};
+// Start the server
+app.listen(port, HOST, () => {
+  console.log('Server is running on PORT: ${port} and HOST: ${HOST}');
+});
+
+
+//front end react
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-function App() {
-  const [location, setLocation] = useState('');
+const App = () => {
+  const [zipCode, setZipCode] = useState('');
   const [weatherData, setWeatherData] = useState(null);
+  const [unit, setUnit] = useState('C');
 
-  const handleInputChange = (event) => {
-    setLocation(event.target.value);
+  useEffect(() => {
+    // Fetch weather data when zipCode changes
+    if (zipCode) {
+      getWeather();
+    }
+  }, [zipCode]);
+
+  const handleZipCodeChange = (event) => {
+    setZipCode(event.target.value);
   };
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const response = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=YOUR_API_KEY&units=metric`
-      );
-      setWeatherData(response.data);
-    } catch (error) {
-      console.error(error);
-    }
+  const handleUnitChange = () => {
+    setUnit(unit === 'C' ? 'F' : 'C');
   };
 
   return (
-    <div className="App">
+    <div>
       <h1>Weather App</h1>
-      <form onSubmit={handleSubmit}>
+      <div style="background-image: url(BG1.jpg);">
+        <label htmlFor="zipCode">Enter ZIP code:</label>
         <input
           type="text"
-          placeholder="Enter a city"
-          value={location}
-          onChange={handleInputChange}
+          name="weatherzipcode"
+          id="zipCode"
+          value={zipCode}
+          onChange={handleZipCodeChange}
         />
-        <button type="submit">Get Weather</button>
-      </form>
-      {weatherData && (
+        <button onClick={fetchWeatherData}>Get Weather</button>
+      </div>
+      { weatherData ? (
         <div>
-          <h2>Weather in {weatherData.name}</h2>
-          <p>Temperature: {weatherData.main.temp}°C</p>
-          <p>Description: {weatherData.weather[0].description}</p>
+          <h2>Current Weather</h2>
+          <p>Location: {weatherData.location.name}</p>
+          <p>Temperature: {weatherData.current.temp_c}°C</p>
+          <p>Condition: {weatherData.current.condition.text}</p>
+          <p>
+            <button onClick={handleUnitChange}>
+              Toggle Unit ({unit === 'C' ? 'Fahrenheit' : 'Celsius'})
+            </button>
+          </p>
         </div>
-      )}
+      ) : null}
     </div>
   );
-}
-
+};
 export default App;
